f6aaf746130e7eab119d3d842961291512f51109
ranand51 <75688202+ranand51@users.noreply.github.com>
2022-07-01 21:09:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "global" {
   backend = "remote"
   config = {
-    organization = "default"
+    organization = "cisco-intersight-demo"
     workspaces = {
       name = var.globalwsname
     }
