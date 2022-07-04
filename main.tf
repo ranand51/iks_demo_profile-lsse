25c112e812580520d04d51f1f90691f9d2daf71d
ranand51 <75688202+ranand51@users.noreply.github.com>
2022-07-04 20:13:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,12 +61,12 @@ data "intersight_ippool_pool" "ippool_moid" {
 
 # Netcfg moids
 data "intersight_kubernetes_network_policy" "netcfg_moid" {
-  name  = local.netcfg_list
+  name  = local.network_pod
 }
 
 # Sysconfig moids
 data "intersight_kubernetes_sys_config_policy" "syscfg_moid" {
-  name  = local.syscfg_list
+  name  = local.network_service
 }
 
 
